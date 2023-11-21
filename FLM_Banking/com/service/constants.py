c8bae155cedcf81e3a183e889dab072aef52993a
Nimmi14 <jaladi.nimmi@gmail.com>
2023-11-21 16:27:31 +0530

    Initial commit

---
+++ FLM_Banking/com/service/constants.py
@@ -0,0 +1,19 @@
+
+MAX_LOGIN_ATTEMPTS = 5
+
+#database configuration
+HOST = "localhost"
+USER = "root"
+PASSWORD = "Nimmi@2810"
+DATABASE = "database_name"
+
+# MAIL CREDENTIALS
+SENDERMAIL = 'bankonline1416@gmail.com' # sample gmail to send otp
+MAILPASS = 'qubz wqfl fiql fbhm'  #better to use app password to avoid stmplib auth error
+
+
+# BANNER PATH
+BANNERPATH = 'C:/Users/JALADI NIMMI/FLMBank/FLM_Banking/com/banner.txt'
+EXITBANNERPATH ='C:/Users/JALADI NIMMI/FLMBank/FLM_Banking/com/exit_banner'
+
+
